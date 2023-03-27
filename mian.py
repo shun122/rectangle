2b4246cce2fd2470ceb5acedfa5e9efb9fd1df8d
shun122 <shunya.aikawa@gmail.com>
2023-03-27 12:52:11 +0900

    first commit

---
+++ mian.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+import scriptcontext as sc
+import os
+import sys
+from imp import reload
+ghdoc = sc.doc
+base_dir = os.path.dirname(ghdoc.Path)
+sys.path.append(base_dir)
+
+#import file
+#reload file
+#from file import file
+
